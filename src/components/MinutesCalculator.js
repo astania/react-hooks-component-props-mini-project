682ba11ef94c33b52d5a84287ab28908e868c04c
+ <+>
2022-01-24 15:35:42 -0500

    Done.

--- src/components/MinutesCalculator.js
+++ src/components/MinutesCalculator.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-function MinutesCalculator({minutes}){
-    const numberOfFiveMinutes = minutes/5
-    if (numberOfFiveMinutes <= 1){
-        return <small>{`☕️ ${minutes} min read`}</small>
-    } else if (numberOfFiveMinutes > 1 && <= 2){
-        return <small>{`🍱 ${minutes} min read`}</small>
-    } else if (numberOfFiveMinutes > 2 && <= 4){
+function MinutesCalculator({ minutes }) {
+    const numberOfFiveMinutes = minutes / 5
+    if (numberOfFiveMinutes <= 1) {
+        return <small>`☕️ ${minutes} min read`</small>
+    } else if (numberOfFiveMinutes > 1 && <= 2) {
+        return <small>`🍱 ${minutes} min read`</small>
+    } else if (numberOfFiveMinutes > 2 && <= 4) {
         return <small>{`🍱🍱 ${minutes} min read`}</small>
-    } else if (numberOfFiveMinutes > 4 && <= 6){
+    } else if (numberOfFiveMinutes > 4 && <= 6) {
         return <small>{`🍱🍱🍱 ${minutes} min read`}</small>
-    } else if (numberOfFiveMinutes > 6 && <= 8){
+    } else if (numberOfFiveMinutes > 6 && <= 8) {
         return <small>{`🍱🍱🍱🍱 ${minutes} min read`}</small>
-
-}
-
-export default MinutesCalculator;
+    }
+    export default MinutesCalculator;
