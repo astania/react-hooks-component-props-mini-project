ec035db2fd5581094c041b0bcc2abedf541c8716
+ <+>
2022-01-24 15:30:06 -0500

    Done.

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,19 +1,12 @@
 import React from "react";
+import MinutesCalculator from "./MinutesCalculator";
 
 function Article({ posts }) {
     return posts.map(({ id, title, date = "January 1, 1970", preview, minutes }) => {
         return <article key={id}>
             <h3>{title}</h3>
             <small>{date}</small>
-            if({minutes} <= 5){
-                <small>{`☕️ ${minutes} min read`}</small>
-            } else if({minutes} > 5 && <= 10){
-                <small>{`☕️☕️ ${minutes} min read`}</small>
-            } else if({minutes} > 10 && <= 15){
-                <small>{`☕️☕️☕️ ${minutes} min read`}</small>
-            } else if({minutes} > 15 && <= 20){
-                <small>{`☕️☕️☕️☕️ ${minutes} min read`}</small>
-            }
+            <MinutesCalculator minutes={minutes}/>
             <p>{preview}</p>
         </article>
     })
